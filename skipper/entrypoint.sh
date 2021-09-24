ec77e1649b6690e1262a98b21c524180a93b78cc
nash <nash@iffy.me>
2021-09-24 16:26:00 +0800

    skipper

---
+++ skipper/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+DAEMON=skipper
+
+piddir=/var/run/$DAEMON
+mkdir -p $piddir
+touch $piddir/$DAEMON.pid
+
+stop() {
+    echo "Received SIGINT or SIGTERM. Shutting down $DAEMON"
+    # Set TERM
+    kill -SIGTERM $(cat $piddir/$DAEMON.pid)
+    # Wait for exit
+    wait $(cat $piddir/$DAEMON.pid)
+    # All done.
+    echo "Done."
+}
+
+trap stop SIGINT SIGTERM
+
+routes=""
+for i in "${!R_@}"; do
+    n=${i:2}
+    r=$(eval "echo \"\$$i\"")
+    routes="${routes}${n}: ${r};"
+done
+
+cmd="/usr/local/bin/skipper -address :80 -inline-routes '${routes}'"
+
+eval "$cmd &"
+pid="$!"
+echo -n "${pid} " >> $piddir/$DAEMON.pid
+
+wait $(cat $piddir/$DAEMON.pid) && exit $?
