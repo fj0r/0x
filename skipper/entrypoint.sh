4eb4d832b988223245897f59a088b17f45b64664
nash <nash@iffy.me>
2021-10-20 13:53:52 +0800

    ssh websocat

--- skipper/entrypoint.sh
+++ skipper/entrypoint.sh
@@ -1,9 +1,87 @@
-#!/bin/bash
-DAEMON=skipper
+#!/usr/bin/env bash
 
-piddir=/var/run/$DAEMON
-mkdir -p $piddir
-touch $piddir/$DAEMON.pid
+set -e
+
+[ "$DEBUG" == 'true' ] && set -x
+
+
+print_fingerprints() {
+    local BASE_DIR=${1-'/etc/ssh'}
+    for item in rsa ecdsa ed25519; do
+        echo ">>> Fingerprints for ${item} host key"
+        ssh-keygen -E md5 -lf ${BASE_DIR}/ssh_host_${item}_key
+        ssh-keygen -E sha256 -lf ${BASE_DIR}/ssh_host_${item}_key
+        ssh-keygen -E sha512 -lf ${BASE_DIR}/ssh_host_${item}_key
+    done
+}
+
+ssh_init() {
+    env | grep _ >> /etc/environment
+
+    if [[ "${SSH_OVERRIDE_HOST_KEYS}" == "true" ]]; then
+        rm -rf /etc/ssh/ssh_host_*
+    fi
+    # Generate Host keys, if required
+    if ls /etc/ssh/ssh_host_* 1> /dev/null 2>&1; then
+        echo ">> Host keys exist in default location"
+        # Don't do anything
+        print_fingerprints
+    else
+        echo ">> Generating new host keys"
+        ssh-keygen -A
+        print_fingerprints /etc/ssh
+    fi
+
+    # Fix permissions, if writable
+    if [ -w ~/.ssh ]; then
+        chown root:root ~/.ssh && chmod 700 ~/.ssh/
+    fi
+    if [ -w ~/.ssh/authorized_keys ]; then
+        chown root:root ~/.ssh/authorized_keys
+        chmod 600 ~/.ssh/authorized_keys
+    fi
+    if [ -w /etc/authorized_keys ]; then
+        chown root:root /etc/authorized_keys
+        chmod 755 /etc/authorized_keys
+        find /etc/authorized_keys/ -type f -exec chmod 644 {} \;
+    fi
+
+    # Add users if SSH_USERS=user:uid:gid set
+    if [ -n "${SSH_USERS}" ]; then
+        USERS=$(echo $SSH_USERS | tr "," "\n")
+        for U in $USERS; do
+            IFS=':' read -ra UA <<< "$U"
+            _NAME=${UA[0]}
+            _UID=${UA[1]}
+            _GID=${UA[2]}
+
+            echo ">> Adding user ${_NAME} with uid: ${_UID}, gid: ${_GID}."
+            if [ ! -e "/etc/authorized_keys/${_NAME}" ]; then
+                echo "WARNING: No SSH authorized_keys found for ${_NAME}!"
+            fi
+            getent group ${_NAME} >/dev/null 2>&1 || groupadd -g ${_GID} ${_NAME}
+            getent passwd ${_NAME} >/dev/null 2>&1 || useradd -r -m -p '' -u ${_UID} -g ${_GID} -s '' -c 'SSHD User' ${_NAME}
+        done
+    else
+        # Warn if no authorized_keys
+        if [ ! -e ~/.ssh/authorized_keys ] && [ ! $(ls -A /etc/authorized_keys) ]; then
+            echo "WARNING: No SSH authorized_keys found!"
+        fi
+    fi
+
+    # Unlock root account, if enabled
+    if [[ "${SSH_ENABLE_ROOT}" == "true" ]]; then
+        usermod -p '' root
+    else
+        echo "WARNING: root account is now locked by default. Set SSH_ENABLE_ROOT=true to unlock the account."
+    fi
+
+    # Update MOTD
+    if [ -v MOTD ]; then
+        echo -e "$MOTD" > /etc/motd
+    fi
+
+}
 
 stop() {
     echo "Received SIGINT or SIGTERM. Shutting down $DAEMON"
@@ -17,6 +95,23 @@ stop() {
 
 trap stop SIGINT SIGTERM
 
+piddir=/var/run/$DAEMON
+mkdir -p $piddir
+
+###########################
+ssh_init
+/usr/sbin/sshd -D -e &
+pid="$!"
+echo -n "${pid}" >> $piddir/sshd.pid
+
+###########################
+/usr/local/bin/websocat -E -b ws-l:127.0.0.1:9999 tcp:127.0.0.1:22 &
+pid="$!"
+echo -n "${pid}" >> $piddir/websocat.pid
+
+###########################
+DAEMON=skipper
+
 routes=""
 for i in "${!R_@}"; do
     n=${i:2}
@@ -24,10 +119,11 @@ for i in "${!R_@}"; do
     routes="${routes}${n}: ${r};"
 done
 
-cmd="/usr/local/bin/skipper -address :80 -wait-for-healthcheck-interval 0 -inline-routes '${routes}'"
+cmd="/usr/local/bin/skipper -address :80 -wait-for-healthcheck-interval 0 -routes-file /eskip -inline-routes '${routes}'"
 
 eval "$cmd &"
 pid="$!"
-echo -n "${pid} " >> $piddir/$DAEMON.pid
+echo -n "${pid}" >> $piddir/$DAEMON.pid
+###########################
 
 wait $(cat $piddir/$DAEMON.pid) && exit $?
