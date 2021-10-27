b0b8c3ee04b300add227160ced3615e3c17ea0fb
nash <nash@iffy.me>
2021-10-27 10:20:37 +0800

    netmaker

---
+++ wg/entrypoint.nm.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+/usr/local/bin/netclient join --daemon off -t ${NM_TOKEN}
+
+DAEMON=socat
+
+piddir=/var/run/$DAEMON
+mkdir -p $piddir
+touch $piddir/$DAEMON.pid
+
+stop() {
+    echo "Received SIGINT or SIGTERM. Shutting down $DAEMON"
+    # Set TERM
+    kill -SIGTERM $(cat $piddir/$DAEMON.pid)
+    # Wait for exit
+    wait $(cat $piddir/$DAEMON.pid)
+    # All done.
+    echo "Done."
+}
+
+trap stop SIGINT SIGTERM
+for i in "${!_@}"; do
+    port=${i:1}
+    if [ ! -z "$port" ]; then
+        url=$(eval "echo \"\$$i\"")
+        cmd="socat tcp-listen:$port,reuseaddr,fork tcp:$url"
+        eval "$cmd &"
+        pid="$!"
+        echo "listen:$port --> $url"
+        echo -n "${pid} " >> $piddir/$DAEMON.pid
+    fi
+done
+
+wait $(cat $piddir/$DAEMON.pid) && exit $?
