8076d2338bca95b7048bc0f1186ac29aaeeb1ac4
nash <nash@iffy.me>
2021-11-13 10:27:34 +0800

    nebula

---
+++ nebula/entrypoint.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+/usr/local/bin/nebula -config ${CONFIGFILE:-/config.yaml}
+addr=$(ip addr show nebula1 | awk 'NR==3 {print $2}' | cut -d'/' -f 1)
+
+DAEMON=socat
+
+piddir=/var/run/$DAEMON
+mkdir -p $piddir
+touch $piddir/$DAEMON.pid
+
+stop() {
+    echo "Received SIGINT or SIGTERM. Shutting down $DAEMON"
+    # Set TERM
+    kill -SIGTERM $(cat $piddir/$DAEMON.pid)
+    # Wait for exit
+    wait $(cat $piddir/$DAEMON.pid)
+    # All done.
+    echo "Done."
+}
+
+trap stop SIGINT SIGTERM
+for i in "${!_@}"; do
+    port=${i:1}
+    if [ ! -z "$port" ]; then
+        url=$(eval "echo \"\$$i\"")
+        cmd="socat tcp-listen:$port,reuseaddr,fork tcp:$url"
+        eval "$cmd &"
+        pid="$!"
+        echo "$addr:$port --> $url"
+        echo -n "${pid} " >> $piddir/$DAEMON.pid
+    fi
+done
+
+wait $(cat $piddir/$DAEMON.pid) && exit $?
