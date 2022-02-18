ac9f1f95ab61070b19fe8bf5cbd54a551d149b13
nash <nash@iffy.me>
2022-02-18 23:20:22 +0800

    skipper +dropbear

--- skipper/entrypoint.sh
+++ skipper/entrypoint.sh
@@ -3,7 +3,41 @@ if [ ! -z $STARTUP_SCRIPT ]; then
   bash $STARTUP_SCRIPT
 fi
 
-DAEMON=skipper
+set_user () {
+    IFS=':' read -ra UA <<< "$1"
+    _NAME=${UA[0]}
+    _UID=${UA[1]:-1000}
+    _GID=${UA[2]:-1000}
+
+    getent group ${_NAME} >/dev/null 2>&1 || groupadd -g ${_GID} ${_NAME}
+    getent passwd ${_NAME} >/dev/null 2>&1 || useradd -m -u ${_UID} -g ${_GID} -G sudo -s /bin/zsh -c "$2" ${_NAME}
+}
+
+init_ssh () {
+    if [ -n "$user" ]; then
+        for u in $(echo $user | tr "," "\n"); do
+            set_user ${u} 'SSH User'
+        done
+    fi
+
+    for i in "${!ed25519_@}"; do
+        _AU=${i:8}
+        _HOME_DIR=$(getent passwd ${_AU} | cut -d: -f6)
+        mkdir -p ${_HOME_DIR}/.ssh
+        eval "echo \"ssh-ed25519 \$$i\" >> ${_HOME_DIR}/.ssh/authorized_keys"
+        chown ${_AU} -R ${_HOME_DIR}/.ssh
+        chmod go-rwx -R ${_HOME_DIR}/.ssh
+    done
+
+    # Fix permissions, if writable
+    if [ -w ~/.ssh ]; then
+        chown root:root ~/.ssh && chmod 700 ~/.ssh/
+    fi
+    if [ -w ~/.ssh/authorized_keys ]; then
+        chown root:root ~/.ssh/authorized_keys
+        chmod 600 ~/.ssh/authorized_keys
+    fi
+}
 
 stop() {
     # Get PID
@@ -18,7 +52,16 @@ stop() {
     echo "Done."
 }
 
+env | grep -E '_|HOME|ROOT|PATH|VERSION|LANG|TIME|MODULE|BUFFERED' \
+    | grep -Ev '^(_|HOME|USER)=' \
+   >> /etc/environment
+
 trap stop SIGINT SIGTERM
+
+init_ssh
+/usr/bin/dropbear -REFms -p 22 &
+echo -n "$! " >> /var/run/services
+
 
 routes=""
 for i in "${!R_@}"; do
