cc80b4c1b55161f3d52ec84f2b1e3e1215e5bbe8
nash <nash@iffy.me>
2022-12-27 12:33:45 +0800

    upgrade postgres docker-entrypoint

---
+++ postgres/mod.sh
@@ -0,0 +1,34 @@
+FNF="$(mktemp)"
+cat <<- EOF > $FNF
+pg_setup_conf() {
+	echo "## Setup \"$PGDATA/usr.conf\""
+	echo "" > $PGDATA/usr.conf
+
+	for i in "${!PGCONF_@}"; do
+		local k=$(echo ${i:7} | tr '[:upper:]' '[:lower:]' | sed 's!__!.!g')
+		local v=$(eval "echo \"\$$i\"")
+        if [ -n "$v" ]; then
+			echo "$k = $v" >> $PGDATA/usr.conf
+		fi
+	done
+	echo "pg_stat_statements.max = 10000" >> $PGDATA/usr.conf
+	echo "pg_stat_statements.track = all" >> $PGDATA/usr.conf
+}
+
+initialize_password() {
+    if [ -z "$POSTGRES_PASSWORD" ]; then
+        export POSTGRES_PASSWORD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 19)
+        echo
+        echo "export \$POSTGRES_PASSWORD=${POSTGRES_PASSWORD}"
+        echo
+    fi
+}
+EOF
+
+sed -e "/docker_verify_minimum_env$/i initialize_password" \
+    -e "/pg_setup_hba_conf \"\$@\"$/a echo \"include_if_exists = 'usr.conf'\">> \$PGDATA/postgresql.conf" \
+    -e "/exec \"\$@\"$/i pg_setup_conf" \
+    -e '/^_main/i cat $FNF' \
+    docker-entrypoint.sh.origin
+
+rm -f $FNF
