a1f2c7d9a75c10899669b4898e1da15033859ae5
nash <nash@iffy.me>
2023-11-15 17:43:23 +0800

    reproxy TIMEOUT WEB_ROOT

--- reproxy/entrypoint/reproxy.sh
+++ reproxy/entrypoint/reproxy.sh
@@ -14,7 +14,17 @@ if [ -n "$ROUTEFILE" ]; then
     routefile="--file.enabled --file.name=${ROUTEFILE}"
 fi
 
-cmd="reproxy ${routefile} ${routes}"
+assets=""
+if [ -n "$WEB_ROOT" ]; then
+    assets="--assets.location=${WEB_ROOT}"
+fi
+
+timeout=""
+for t in read-header write idle dial keep-alive resp-header idle-conn tls continue; do
+    timeout="${timeout} --timeout.${t}=${TIMEOUT:-0}"
+done
+
+cmd="reproxy --listen 0.0.0.0:80 ${assets} ${routefile} ${routes} ${timeout}"
 
 eval "$cmd 2>&1 &"
 echo -n "$! " >> /var/run/services
