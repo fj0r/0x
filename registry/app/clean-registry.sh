9d0eaacba42cb0ec55fd96e9c84dd5dada2ea442
nash <nash@iffy.me>
2024-04-17 10:30:56 +0800

    registry garbage-collect

--- registry/app/clean-registry.sh
+++ registry/app/clean-registry.sh
@@ -8,3 +8,5 @@ else
     curl -sSL localhost:5000/admin/deletion?retain=$r \
     | curl -sSL -X POST localhost:5000/admin/deletion --data-binary @-
 fi
+
+/usr/local/bin/registry garbage-collect --delete-untagged /etc/docker/registry/config.yml
