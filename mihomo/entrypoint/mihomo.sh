2a8878c097531bab20590c78ce2bd3ccdef1c39c
nash <nash@iffy.me>
2024-12-17 18:25:49 +0800

    watchexec

--- mihomo/entrypoint/mihomo.sh
+++ mihomo/entrypoint/mihomo.sh
@@ -2,9 +2,9 @@ for i in Country.mmdb geoip.dat geoip.db; do
     ln -fs /opt/${i} /data
 done
 
-if [ -n "$WATCH_ALL" ]; then
-    watchexec -r -w /data/ -e yaml,yml -- mihomo -d /data -ext-ctl 0.0.0.0:9090 2>&1 &
-else
-    watchexec -r -w /data/config.yaml -- mihomo -d /data -ext-ctl 0.0.0.0:9090 2>&1 &
-fi
+watchexec -r \
+    -w /data/config.yaml \
+    -w /data/rules \
+    -- mihomo -d /data -ext-ctl 0.0.0.0:9090 2>&1 &
+
 echo -n "$! " >> /var/run/services
