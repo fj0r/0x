9b00abe3a3c8320f6552207088f3bfaf87a7d823
nash <fj0rd@qq.com>
2025-03-28 09:06:46 +0800

    watchexec debounce

--- mihomo/entrypoint/mihomo.sh
+++ mihomo/entrypoint/mihomo.sh
@@ -2,7 +2,7 @@ for i in Country.mmdb geoip.dat geoip.db; do
     ln -fs /opt/${i} /data
 done
 
-watchexec -r \
+watchexec -r --debounce 1000 \
     -w /data/config.yaml \
     -w /data/rules \
     -- mihomo -d /data -ext-ctl 0.0.0.0:9090 2>&1 &
